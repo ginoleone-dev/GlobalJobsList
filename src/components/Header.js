494720b93b1cbaebef20ec68b05d4ca99df4dd6c
gino leone <ginoleone1998@gmail.com>
2022-07-12 11:12:31 -0600

    responsive part1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
-import React from "react";
+import "./headerFooter.css"
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 export default function Header () {
+
+const [openMenu, setOpenMenu] = useState(false)
+
+function toggleMenu(){
+  setOpenMenu(menuStatus => !menuStatus)
+}
+
 return(
  <nav className="navBar">
     <h1 className="pageTitle">GlobalJobsList</h1>
-    <ul className="mainNav">
-      <Link to="/" className="navItem">Home</Link>
-      <Link to="/about" className="navItem">About</Link>
-      <Link to="/contact" className="navItem">Contact</Link>
+    <ul className={openMenu ? "mainNav expanded" : "mainNav"}>
+      <Link to="/" className="navItem" onClick={toggleMenu}>Home</Link>
+      <Link to="/about" className="navItem" onClick={toggleMenu}>About</Link>
+      <Link to="/contact" className="navItem" onClick={toggleMenu}>Contact</Link>
     </ul>
+      <span className='hamburger'>
+        <a href='#' onClick={toggleMenu}><i className="fa fa-bars"></i></a>
+      </span>      
  </nav>
 )
 
