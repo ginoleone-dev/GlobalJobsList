6b4443ed8729c197f4ea0a198e5a09df92f43b6e
Top <97708528+Topgramming@users.noreply.github.com>
2022-07-27 15:59:46 -0600

    added contact form functionality with firebase

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,110 +1,225 @@
 import * as React from 'react';
-import Footer from "../components/Footer";
-import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material';
-import './Pages.css'
+import { useState, useEffect } from 'react';
+import Footer from '../components/Footer';
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import './Pages.css';
+import { db } from '../firebase-config';
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from 'firebase/firestore';
 
 const textAreaStyle = {
-    position: 'absolute',
+  position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
   border: '2px solid #000',
-  marginTop: -50 ,
+  marginTop: -50,
   boxShadow: 24,
   p: 4,
-}
-
-
+};
 
 export default function Contact() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-  // const [formData, setFormData] = useState({
-  //   firstname:"",
-  //   lastName: "", 
-  //   email:"", 
-  //   comments:"",
-  //   isFriendly: true,
-  // })
+  const contactsCollectionRef = collection(db, 'contacts');
 
+  // Connecting to the db on page load
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(contactsCollectionRef);
+      console.log(data.docs);
+    };
+    getUsers();
+  }, []);
 
-  // function handleChange(event){
-  //   const {name, value, type, checked} = event.target
-  //   setFormData(prevFormData => {
-  //     return {
-  //       ...prevFormData,
-  //       [name] : type === "checkbox" ? checked : value
-  //     }
-  //   })
-  // }
-const fieldStyles = {}
+  // Create a contact
+  const createContact = async () => {
+    await addDoc(contactsCollectionRef, {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      message: message,
+    });
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setMessage('');
+  };
 
+  // const emptyFields = () => {
+  //   setFirstName('');
+  //   setLastName('');
+  // };
   return (
-      <>
+    <>
       {/* Outer Card */}
-<Card style={{maxWidth:550, margin:"0 auto", padding:"5rem 0px", backgroundColor: "transparent", boxShadow:"none",  height:"100vh"}}>
-  <CardContent style={{ maxWidth:550 ,backgroundColor:"#0d6efd", borderRadius:"20px"}} >
+      <Card
+        style={{
+          maxWidth: 550,
+          margin: '0 auto',
+          padding: '5rem 0px',
+          backgroundColor: 'transparent',
+          boxShadow: 'none',
+          height: '100vh',
+        }}
+      >
+        <CardContent
+          style={{
+            maxWidth: 550,
+            backgroundColor: '#0d6efd',
+            borderRadius: '20px',
+          }}
+        >
+          <Typography
+            gutterBottom
+            variant="h5"
+            style={{
+              fontFamily: 'inter',
+              color: '#ffffff',
+              margin: '10px 5px',
+              fontSize: '2rem',
+              textAlign: 'center',
+              height: '50px',
+            }}
+          >
+            Contact Us
+          </Typography>
+          <Typography
+            variant="body2"
+            component="p"
+            style={{
+              fontFamily: 'inter',
+              color: '#ffffff',
+              margin: '10px 5px',
+            }}
+          >
+            I would like to know any possible improvement that you can think of!
+          </Typography>
 
-    <Typography gutterBottom variant='h5' style={{fontFamily: "inter", color:"#ffffff", margin:"10px 5px", fontSize:"2rem", textAlign:"center", height:"50px"}}>Contact Us</Typography>
-    <Typography variant="body2" component="p" style={{fontFamily: "inter", color:"#ffffff", margin:"10px 5px"}}>I would like to know any possible improvement that you can think of!</Typography>
+          {/* Outer Grid */}
+          <Grid container spacing={1}>
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="First Name"
+                placeholder="Enter first name"
+                variant="filled"
+                value={firstName}
+                onChange={(event) => {
+                  setFirstName(event.target.value);
+                }}
+                style={{
+                  backgroundColor: '#edede9',
+                  borderRadius: 12,
+                  marginBottom: 15,
+                }}
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="Last Name"
+                placeholder="Enter last name"
+                variant="filled"
+                name="lastName"
+                value={lastName}
+                onChange={(event) => {
+                  setLastName(event.target.value);
+                }}
+                style={{
+                  backgroundColor: '#edede9',
+                  borderRadius: 12,
+                  marginBottom: 15,
+                }}
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                type="email"
+                label="Email"
+                placeholder="Enter email name"
+                variant="filled"
+                name="email"
+                value={email}
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+                style={{
+                  backgroundColor: '#edede9',
+                  borderRadius: 12,
+                  marginBottom: 15,
+                }}
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                type="message"
+                multiline
+                rows={4}
+                label="Message"
+                placeholder="Type your message"
+                name="message"
+                value={message}
+                onChange={(event) => {
+                  setMessage(event.target.value);
+                }}
+                style={{
+                  backgroundColor: '#edede9',
+                  borderRadius: 12,
+                  marginBottom: 15,
+                }}
+                variant="filled"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Button
+                variant="contained"
+                align="center"
+                style={{
+                  backgroundColor: '#4c5c68',
+                  borderRadius: '5px',
+                  margin: '0 auto',
+                  display: 'flex',
+                  height: 50,
+                  width: 200,
+                }}
+                onClick={() => {
+                  createContact();
+                }}
+                fullWidth
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
 
-      {/* Outer Grid */}
-  <Grid container spacing={1}>
-
-      <Grid xs={12} sm={6} item>
-        <TextField 
-         label="First Name" 
-         placeholder='Enter first name' 
-         variant='filled' 
-         style={{ backgroundColor:"#edede9", borderRadius:12, marginBottom:15}} 
-         fullWidth required
-         />
-      </Grid>
-       <Grid xs={12} sm={6} item>
-        <TextField 
-        label="Last Name" 
-        placeholder='Enter last name' 
-         variant='filled'
-        style={{ backgroundColor:"#edede9", borderRadius:12, marginBottom:15}} 
-        fullWidth required
-        />
-       </Grid>
-       <Grid xs={12} item>
-        <TextField 
-        type='email' 
-        label="Email" 
-        placeholder='Enter email name' 
-         variant='filled'
-        style={{ backgroundColor:"#edede9", borderRadius:12, marginBottom:15}} 
-        fullWidth required
-        />        
-       </Grid>
-      <Grid xs={12} item>
-        <TextField 
-        type='message' 
-        multiline rows={4} 
-        label="Message" 
-        placeholder='Type your message' 
-        style={{ backgroundColor:"#edede9", borderRadius:12, marginBottom:15}} 
-         variant='filled'
-        fullWidth required/>        
-      </Grid>
-      <Grid xs={12} item>
-        <Button
-         variant='contained' 
-         align='center'
-         style={{backgroundColor:"#4c5c68", borderRadius: "5px", margin: '0 auto', display:"flex",height: 50, width:200, }} 
-         fullWidth
-         >
-          Submit
-         </Button>
-      </Grid>
-    </Grid>
-  </CardContent>
-</Card>
-
-<Footer/>
-      </>
-  )
+      <Footer />
+    </>
+  );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from '@firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAzdeagVvLlFZR50v99q_EivEK7Fp1OSVA',
+  authDomain: 'jobs-db-d56b6.firebaseapp.com',
+  projectId: 'jobs-db-d56b6',
+  storageBucket: 'jobs-db-d56b6.appspot.com',
+  messagingSenderId: '950678935764',
+  appId: '1:950678935764:web:fba630f862651a6c0a0341',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
